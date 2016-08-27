552911dff385c2b1cbd4c14a0ef09a1728188e0d
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-08-27 00:07:49 -0700

    Insane number parsing code allows mangling Java, C
    Also, this supports the intended behavior of the character mapping
    changing unpredictably when the number of identifiers changes in a file,
    making editing even harder.

--- src/main/java/trash/panda/Scavenging.java
+++ src/main/java/trash/panda/Scavenging.java
@@ -21,7 +21,6 @@ public class Scavenging {
      */
     private static final long INV_LONG_PHI = 0xF1DE83E19937733DL;
 
-
     /**
      * Quickly mixes the bits of a long integer.
      * <br>This method mixes the bits of the argument by multiplying by the golden ratio and
@@ -50,14 +49,16 @@ public class Scavenging {
 
     public static final Pattern identifierPattern =
             Pattern.compile("(?<!\\p{Jp})\\p{Js}\\p{Jp}*"),
-    numberPattern =
-            Pattern.compile("(?<!\\p{Jp})-?(?=[1-9]|0(?!\\d))\\d+(\\.\\d+)?([eE][+-]?\\d+)?");
+            numberPattern =
+                    Pattern.compile("(?<!\\p{Jp})(?:" +
+                            "({=INT}(?:({=BASE8}-?(?=0)[0-9]+)|({=BASE10}-?[1-9][0-9]*)|({=BASE16}-?0[Xx][0-9A-Fa-f]+))" +
+                            "({=INTSUFFIX}(?:[Uu]?[Nn]?[Ll]{0,2})))|" +
+                            "({=REAL}({=REALBODY}-?(?=[1-9]|0(?![0-9]))[0-9]+(\\.[0-9]+)?([Ee][+-]?[0-9]+)?)({=REALSUFFIX}[FfMm]?)))");
     public static char[] identifierStarts = Category.IdentifierStart.contents(),
             identifierParts = Category.IdentifierPart.contents();
 
-    private static String scavenge(String original, CharCharMap startMapping, CharCharMap restMapping)
-    {
-        if(original == null || original.isEmpty())
+    private static String scavenge(String original, CharCharMap startMapping, CharCharMap restMapping) {
+        if (original == null || original.isEmpty())
             return "";
         char[] trash = new char[original.length()];
         trash[0] = startMapping.get(original.charAt(0));
@@ -68,36 +69,81 @@ public class Scavenging {
     }
 
     /**
-     * Takes a String representing a number, reads in the number, takes a figurative shotgun to the bits, and
-     * re-encodes to double.
-     * @param original a String storing a double
-     * @return a mangled version of the original double.
+     * Takes a MatchResult matching a number, reads in the number, takes a figurative shotgun to the bits, and
+     * re-encodes to the same numeric type before returning as a String.
+     *
+     * @param original a MatchResult matching a number
+     * @return a mangled version of the original number, as a String
      */
-    private static String shotgun(String original)
-    {
-        return String.valueOf(Double.longBitsToDouble(mix(Double.doubleToLongBits(Double.parseDouble(original)))));
+    private static String shotgun(MatchResult original) {
+        String check = original.group("REAL"), suffix;
+        if (check != null && !check.isEmpty()) {
+            suffix = original.group("REALSUFFIX");
+            check = original.group("REALBODY");
+            return String.valueOf(Double.longBitsToDouble(mix(Double.doubleToLongBits(Double.parseDouble(check))))) + suffix;
+        }
+        check = original.group("INT");
+        if (check != null && !check.isEmpty()) {
+            suffix = original.group("INTSUFFIX");
+            check = original.group("BASE8");
+            if (check != null && !check.isEmpty()) {
+                return String.valueOf(mix(Long.parseLong(check, 8) + INV_LONG_PHI)) + suffix;
+            }
+            check = original.group("BASE10");
+            if (check != null && !check.isEmpty()) {
+                return String.valueOf(mix(Long.parseLong(check) + INV_LONG_PHI)) + suffix;
+            }
+            check = original.group("BASE16");
+            if (check != null && !check.isEmpty()) {
+                return String.valueOf(mix(Long.parseLong(check, 16) + INV_LONG_PHI)) + suffix;
+            }
+        }
+        return original.group();
     }
 
     /**
-     * Takes a String representing a number that has been blown apart by {@link #shotgun(String)}, reads in the
-     * number, reconstitutes the bits as if making a fur hat, and re-encodes to double.
+     * Takes a MatchResult matching a number that has been blown apart by {@link #shotgun(MatchResult)}, reads
+     * in the number, reconstitutes the bits as if making a fur hat, and re-encodes to the correct type of
+     * number as a String.
+     *
      * @param original a String storing a double
      * @return a de-mangled version of the original double.
      */
-    private static String hat(String original)
-    {
-        return String.valueOf(Double.longBitsToDouble(invMix(Double.doubleToLongBits(Double.parseDouble(original)))));
+    private static String hat(MatchResult original) {
+        String check = original.group("REAL"), suffix;
+        if (check != null && !check.isEmpty()) {
+            suffix = original.group("REALSUFFIX");
+            check = original.group("REALBODY");
+            return String.valueOf(Double.longBitsToDouble(invMix(Double.doubleToLongBits(Double.parseDouble(check))))) + suffix;
+        }
+        check = original.group("INT");
+        if (check != null && !check.isEmpty()) {
+            suffix = original.group("INTSUFFIX");
+            check = original.group("BASE8");
+            if (check != null && !check.isEmpty()) {
+                return String.valueOf(invMix(Long.parseLong(check, 8)) - INV_LONG_PHI) + suffix;
+            }
+            check = original.group("BASE10");
+            if (check != null && !check.isEmpty()) {
+                return String.valueOf(invMix(Long.parseLong(check)) - INV_LONG_PHI) + suffix;
+            }
+            check = original.group("BASE16");
+            if (check != null && !check.isEmpty()) {
+                return String.valueOf(invMix(Long.parseLong(check, 16)) - INV_LONG_PHI) + suffix;
+            }
+        }
+        return original.group();
     }
 
-    public static String scramble(String input, long seed)
-    {
+    public static String scramble(String input, long seed) {
         StatefulRNG iRNG = new StatefulRNG(seed),
                 nRNG = new StatefulRNG(iRNG.nextLong()),
-                oRNG = new StatefulRNG(nRNG.nextLong());
+                oRNG;
         Matcher match = identifierPattern.matcher(input);
         ArrayList<String> segments = match.foundStrings();
         int ilen = identifierParts.length;
         int[] mixer = iRNG.randomOrdering(segments.size());
+        oRNG = new StatefulRNG(iRNG.nextLong());
         int[] megaMix = oRNG.randomOrdering(ilen);
         CharCharMap partMapping = new CharCharMap(ilen);
         for (int i = 0; i < ilen; i++) {
@@ -110,25 +156,25 @@ public class Scavenging {
             startMapping.put(identifierStarts[i], identifierStarts[megaMix[i]]);
         }
         match.setPosition(0);
-        for(int i = 0; i < mixer.length; i++)
-        {
+        for (int i = 0; i < mixer.length; i++) {
             segments.set(i, scavenge(segments.get(i), startMapping, partMapping));
         }
         Scrambler scram = new Scrambler(mixer, segments);
         String altered = match.replaceAll(scram);
         match = numberPattern.matcher(altered);
-        segments = match.foundStrings();
+        segments.clear();
+        MatchIterator it = match.findAll();
+        while (it.hasNext()) {
+            segments.add(shotgun(it.next()));
+        }
         mixer = nRNG.randomOrdering(segments.size());
         match.setPosition(0);
-        for(int i = 0; i < mixer.length; i++)
-        {
-            segments.set(i, shotgun(segments.get(i)));
-        }
+
         scram.reset(mixer, segments);
         return match.replaceAll(scram);
     }
-    private static class Scrambler implements Substitution
-    {
+
+    private static class Scrambler implements Substitution {
         private int counter;
         private int[] ordering;
         private ArrayList<String> segments;
@@ -138,6 +184,7 @@ public class Scavenging {
             this.segments = segments;
             counter = 0;
         }
+
         public void reset(int[] ordering, ArrayList<String> segments) {
             this.ordering = ordering;
             this.segments = segments;
@@ -150,15 +197,15 @@ public class Scavenging {
         }
     }
 
-    public static String unscramble(String input, long seed)
-    {
+    public static String unscramble(String input, long seed) {
         StatefulRNG iRNG = new StatefulRNG(seed),
                 nRNG = new StatefulRNG(iRNG.nextLong()),
-                oRNG = new StatefulRNG(nRNG.nextLong());
+                oRNG;
         Matcher match = identifierPattern.matcher(input);
         ArrayList<String> segments = match.foundStrings();
         int ilen = identifierParts.length;
         int[] mixer = Compatibility.invertOrdering(iRNG.randomOrdering(segments.size()));
+        oRNG = new StatefulRNG(iRNG.nextLong());
         int[] megaMix = oRNG.randomOrdering(ilen);
         CharCharMap partMapping = new CharCharMap(ilen);
         for (int i = 0; i < ilen; i++) {
@@ -171,20 +218,22 @@ public class Scavenging {
             startMapping.put(identifierStarts[megaMix[i]], identifierStarts[i]);
         }
         match.setPosition(0);
-        for(int i = 0; i < mixer.length; i++)
-        {
+        for (int i = 0; i < mixer.length; i++) {
             segments.set(i, scavenge(segments.get(i), startMapping, partMapping));
         }
         Scrambler scram = new Scrambler(mixer, segments);
         String altered = match.replaceAll(scram);
+
         match = numberPattern.matcher(altered);
-        segments = match.foundStrings();
+        segments.clear();
+        MatchIterator it = match.findAll();
+        while (it.hasNext()) {
+            segments.add(hat(it.next()));
+        }
         mixer = Compatibility.invertOrdering(nRNG.randomOrdering(segments.size()));
         match.setPosition(0);
-        for(int i = 0; i < mixer.length; i++)
-        {
-            segments.set(i, hat(segments.get(i)));
-        }
+
         scram.reset(mixer, segments);
         return match.replaceAll(scram);
-    }}
+    }
+}
--- src/test/java/trash/panda/ScavengingTest.java
+++ src/test/java/trash/panda/ScavengingTest.java
@@ -1,5 +1,6 @@
 package trash.panda;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,27 +11,32 @@ import java.io.InputStream;
  */
 public class ScavengingTest {
     @Test
-    public void testEncodeSample()
-    {
-        InputStream is = ScavengingTest.class.getResourceAsStream("/sample.json");
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        String inp = s.hasNext() ? s.next() : "";
-        try {
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void testEncodeSample() {
+        for (String name : new String[]{"/sample.json", "/sample.java"}) {
+            InputStream is = ScavengingTest.class.getResourceAsStream(name);
+            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+            String inp = s.hasNext() ? s.next() : "";
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(inp);
+            System.out.println("\n");
+            String scrambled = Scavenging.scramble(inp, 0xBEEFBEEFBEEFBEEFL), unscrambled;
+            System.out.println(scrambled);
+            System.out.println("\n");
+            unscrambled = Scavenging.unscramble(scrambled, 0xBEEFBEEFBEEFBEEFL);
+            System.out.println(unscrambled);
+            System.out.println("\n");
+            Assert.assertEquals(inp, unscrambled);
+            scrambled = Scavenging.scramble(inp, 0L);
+            System.out.println(scrambled);
+            System.out.println("\n");
+            unscrambled = Scavenging.unscramble(scrambled, 0L);
+            System.out.println(unscrambled);
+            System.out.println("\n");
+            Assert.assertEquals(inp, unscrambled);
         }
-        System.out.println(inp);
-        System.out.println("\n");
-        String scrambled = Scavenging.scramble(inp, 0xBEEFBEEFBEEFBEEFL);
-        System.out.println(scrambled);
-        System.out.println("\n");
-        System.out.println(Scavenging.unscramble(scrambled, 0xBEEFBEEFBEEFBEEFL));
-        System.out.println("\n");
-        scrambled = Scavenging.scramble(inp, 0L);
-        System.out.println(scrambled);
-        System.out.println("\n");
-        System.out.println(Scavenging.unscramble(scrambled, 0L));
-        System.out.println("\n");
     }
 }
